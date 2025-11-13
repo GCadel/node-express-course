e86881b29877edc49ce8121f406c2f042e2d47d0
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-11-13 19:03:53 +0900

    Prints global variables to screen

---
+++ 01-node-tutorial/answers/02-globals.js
@@ -0,0 +1,9 @@
+// Defined variable in terminal using `export SUPER_SECRET_GLOBAL="George says hello!"`
+// If defined, prints "George says hello!", otherwise, prints "undefined"
+console.log(process.env.SUPER_SECRET_GLOBAL);
+
+// Prints the path of the current directory
+console.log(__dirname);
+
+// Prints the FormData class
+console.log(FormData);
