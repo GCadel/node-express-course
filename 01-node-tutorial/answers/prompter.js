db2a9af8a7e6fd5ab0ea69571ca20c4ab8503ede
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-11-17 00:05:59 +0900

    Update form to search for national parks matching user query

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -44,8 +44,79 @@ const server = http.createServer((req, res) => {
     getBody(req, (body) => {
       console.log("The body of the post is ", body);
       // here, you can add your own logic
+      // List from https://copypastelist.com/nature/list/list-of-national-parks-in-the-us
+      const nationalParks = [
+        "Acadia",
+        "American Samoa",
+        "Arches",
+        "Badlands",
+        "Big Bend",
+        "Biscayne",
+        "Black Canyon of the Gunnison",
+        "Bryce Canyon",
+        "Canyonlands",
+        "Capitol Reef",
+        "Carlsbad Caverns",
+        "Channel Islands",
+        "Congaree",
+        "Crater Lake",
+        "Cuyahoga Valley",
+        "Death Valley",
+        "Denali",
+        "Dry Tortugas",
+        "Everglades",
+        "Gates of the Arctic",
+        "Gateway Arch",
+        "Glacier",
+        "Glacier Bay",
+        "Grand Canyon",
+        "Grand Teton",
+        "Great Basin",
+        "Great Sand Dunes",
+        "Great Smoky Mountains",
+        "Guadalupe Mountains",
+        "Haleakalā",
+        "Hawaiʻi Volcanoes",
+        "Hot Springs",
+        "Indiana Dunes",
+        "Isle Royale",
+        "Joshua Tree",
+        "Katmai",
+        "Kenai Fjords",
+        "Kings Canyon",
+        "Kobuk Valley",
+        "Lake Clark",
+        "Lassen Volcanic",
+        "Mammoth Cave",
+        "Mesa Verde",
+        "Mount Rainier",
+        "New River Gorge",
+        "North Cascades",
+        "Olympic",
+        "Petrified Forest",
+        "Pinnacles",
+        "Redwood",
+        "Rocky Mountain",
+        "Saguaro",
+        "Sequoia",
+        "Shenandoah",
+        "Theodore Roosevelt",
+        "Virgin Islands",
+        "Voyageurs",
+        "White Sands",
+        "Wind Cave",
+        "Wrangell–St. Elias",
+        "Yellowstone",
+        "Yosemite",
+        "Zion",
+      ];
       if (body["item"]) {
-        item = body["item"];
+        item = `US National Parks containing "${body.item}"</br>
+        ${nationalParks
+          .filter((park) =>
+            park.toLowerCase().includes(body.item.toLowerCase())
+          )
+          .toString()}`;
       } else {
         item = "Nothing was entered.";
       }
