edeaf840c30810d3f0121dcab681c885c1504e8b
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-11-16 22:59:59 +0900

    Updated to write to the file three times

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -1,5 +1,15 @@
 const { writeFileSync, readFileSync } = require("fs");
-writeFileSync("./temporary/fileA.txt", "This will be written to a text file", {
+writeFileSync(
+  "./temporary/fileA.txt",
+  "This will be written to a text file\n",
+  {
+    flag: "a",
+  }
+);
+writeFileSync("./temporary/fileA.txt", "Second line\n", {
+  flag: "a",
+});
+writeFileSync("./temporary/fileA.txt", "Third line", {
   flag: "a",
 });
 console.log(readFileSync("./temporary/fileA.txt", { encoding: "utf8" }));
