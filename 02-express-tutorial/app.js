c1bc9c9a3f2377522ac0a10e4b08f540e3f65ea4
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-12-02 16:04:15 +0900

    Created express server that serves html and json via api

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,48 @@
-console.log('Express Tutorial')
+const express = require("express");
+const { products } = require("./data.js");
+
+const app = express();
+app.use(express.static("./public"));
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked!" });
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const product = products.find(
+    (item) => item.id === parseInt(req.params.productID)
+  );
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "This product does not exist" });
+  }
+});
+
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  // Filter items based on search term
+  const searchProducts = products.filter((item) =>
+    item.name.toLowerCase().includes(req.query.search.toLowerCase())
+  );
+  // Return the first n limit items
+  const limitProducts = searchProducts.slice(0, req.query.limit);
+  res.json(limitProducts);
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Got data!");
+});
+
+app.all("/*", (req, res) => {
+  res.send("Uh oh, we couldn't find that page!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening");
+});
