e9f7565aaf5d9efbd101c25f7cc53afb744c2e05
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-12-22 01:00:57 +0900

    Updated api to query database

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,24 +1,53 @@
-const getAllTasks = (req, res) => {
-  res.status(200).json({ message: "Get All Tasks" });
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.status(200).json(tasks);
 };
 
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
   const taskId = req.params.id;
-  res.status(200).json({ message: `Get Task ${taskId}` });
+  const task = await Task.findById(taskId);
+  if (!task) {
+    res.status(404).json({ message: "Item does not exist" });
+    return;
+  }
+  res.status(200).json(task);
 };
 
-const createTask = (req, res) => {
-  res.status(200).json({ message: "Get All Tasks" });
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Unable to create task", error: error.message });
+  }
 };
 
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
   const taskId = req.params.id;
-  res.status(200).json({ message: `Update Task ${taskId}` });
+  const result = await Task.findByIdAndUpdate(taskId, req.body);
+  if (!result) {
+    res
+      .status(404)
+      .json({ message: "Unable to update task", error: "Task does not exist" });
+    return;
+  }
+  res.status(200).json({ message: `Updated Task ${taskId}` });
 };
 
-const deleteTask = (req, res) => {
+const deleteTask = async (req, res) => {
   const taskId = req.params.id;
-  res.status(200).json({ message: `Delete Task ${taskId}` });
+  const result = await Task.findByIdAndDelete(taskId);
+  if (!result) {
+    res
+      .status(404)
+      .json({ message: "Unable to delete task", error: "Task does not exist" });
+    return;
+  }
+  res.status(200).json({ message: `Deleted Task ${taskId}` });
 };
 
 module.exports = {
