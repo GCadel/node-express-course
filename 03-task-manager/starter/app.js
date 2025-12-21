b3dbe34f35999fda32a37a6359a2cd7e9234d45e
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-12-22 01:01:20 +0900

    Added server startup for successful database connection

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const taskRouter = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
 const PORT = 3000;
 
 app.use("/api/tasks", taskRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.get("/*", (req, res) => {
+  res.status(404).json({ message: "Not Found" });
 });
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO);
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
