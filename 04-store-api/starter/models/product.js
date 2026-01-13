14c18a34a152480629c28ddfe03e13ac5cef323d
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-01-13 22:24:22 +0900

    Created product model

---
+++ 04-store-api/starter/models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Product Name Required"] },
+  price: { type: Number, required: [true, "Product Price Required"] },
+  features: { type: Boolean, default: false },
+  rating: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now() },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+    // enum:["ikea", "liddy", "caressa", "marcos"]
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
