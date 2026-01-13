5d3a2c9e6a7f2909437b539dc3a9caeb5da946ba
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-01-14 01:06:11 +0900

    Add ability to limit results for pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,6 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
+  // const { featured, company, name, sort, fields, limit } = req.query;
   const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
@@ -42,6 +43,25 @@ const getAllProducts = async (req, res) => {
     queryObject.selectedFieldList = defaultFieldList;
     result = result.select(defaultFieldList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  // if (limit) {
+  //   if (isNaN(Number(limit))) {
+  //     queryObject.limit = 10;
+  //     result = result.limit(10);
+  //   } else {
+  //     queryObject.limit = Number(limit);
+  //     result = result.limit(Number(limit));
+  //   }
+  // } else {
+  //   queryObject.limit = 10;
+  //   result = result.limit(10);
+  // }
 
   const products = await result;
   res.status(200).json({
