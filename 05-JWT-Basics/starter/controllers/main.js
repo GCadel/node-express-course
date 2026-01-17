7c678126e774fe9a5e4bf4eedfd45068feb37fd9
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-01-17 21:42:36 +0900

    Add ability to generate jwt and provide data with verified token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,12 +1,46 @@
+const { JsonWebTokenError, sign, decode, verify } = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+const { json } = require("express");
+
 const login = async (req, res) => {
-  res.send("Faux Login/Register/Signup");
+  console.log(req.body);
+  // Check for username and password in body
+  const { username, password } = req.body;
+  if (username && password) {
+    // (Verify user exists in database first) Create a jwt
+    // The ID is a random placeholder. Normally it will be from the db
+    const id = new Date().getDate();
+    const token = sign({ username, id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    console.log(decode(token));
+
+    // send to frontend
+    return res.status(200).json({ msg: `Account created`, token });
+  }
+  throw new CustomAPIError("Missing username or password", 400);
 };
 
 const dashboard = async (req, res) => {
-  const luckyNum = Math.floor(Math.random() * 42);
-  res
-    .status(200)
-    .json({ message: `Hello, Steve`, secret: `Your auth data: ${luckyNum}` });
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No auth token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    console.log(Date(decoded.iat), Date(decoded.exp));
+    const luckyNum = Math.floor(Math.random() * 42);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Your auth data: ${luckyNum}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access route", 401);
+  }
 };
 
 module.exports = { login, dashboard };
