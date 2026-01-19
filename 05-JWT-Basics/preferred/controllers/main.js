0706cb64a75c4b6b374ab52d58c477689a28443f
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-01-20 07:00:12 +0900

    Initialize "preferred" assignment

---
+++ 05-JWT-Basics/preferred/controllers/main.js
@@ -0,0 +1,29 @@
+const { sign } = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    // (Verify user exists in database first) Create a jwt
+    // The ID is a random placeholder. Normally it will be from the db
+    const id = new Date().getDate();
+    const token = sign({ username, id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    // send to frontend
+    return res.status(200).json({ msg: `Account created`, token });
+  }
+  throw new BadRequestError("Missing username or password");
+};
+
+const dashboard = async (req, res) => {
+  const { user } = req;
+  const luckyNum = Math.floor(Math.random() * 42);
+  res.status(200).json({
+    msg: `Hello, ${user.username}`,
+    secret: `Your auth data: ${luckyNum}`,
+  });
+};
+
+module.exports = { login, dashboard };
